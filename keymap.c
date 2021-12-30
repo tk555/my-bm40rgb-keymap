25f41ede259abfc78ff524cf10a9404cdcc898b4
tk555 <19501990+tk555@users.noreply.github.com>
2021-12-31 05:16:44 +0900

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -29,29 +29,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_QWERTY] = LAYOUT_planck_mit(
-    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_ESC,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LCTL,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    XXXXXXX, XXXXXXX, KC_LGUI, LOWER, KC_SPC,   KC_ENT,  RAISE,   KC_LALT,   KC_INS,  KC_PSCR,   KC_PAUS
+    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LALT,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+    KC_ESC, XXXXXXX, KC_LSFT, LOWER, KC_SPC,   KC_ENT,  RAISE,  KC_LCTL ,   KC_INS,  KC_PSCR,   KC_PAUS
 ),
 
 [_LOWER] = LAYOUT_planck_mit(
     LCTL(KC_Q)   , KC_MS_UP, KC_MS_BTN4,    KC_MS_BTN5,LCTL(KC_T), _______,   KC_APP, KC_F7, KC_F8, KC_F9, KC_F10, KC_ESC,
     KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,    KC_MS_BTN1, KC_MS_BTN2,_______,   KC_PGUP, KC_F4, KC_F5, KC_F6, KC_F11, KC_F14,
     LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),    LCTL(KC_V), LCTL(KC_B),_______,   KC_PGDN, KC_F1, KC_F2, KC_F3, KC_F12, KC_F13,
-    _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_mit(
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
       KC_F20,   KC_F21,   KC_F22,  KC_F23,   KC_F24,_______,   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_MINS,  KC_EQL,
       KC_F15,   KC_F16,   KC_F17,  KC_F18,   KC_F19,_______,   KC_BSPC,  KC_DEL, KC_HOME,  KC_END,  KC_GRV, KC_RSFT,
-    _______, _______, _______, _______, KC_TAB, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, KC_TAB, _______, _______, _______, _______, _______, _______
 ),
 
 
 [_ADJUST] = LAYOUT_planck_mit(
-      RESET,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, _______, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+      RESET,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, _______, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
