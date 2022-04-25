a5ce8f5fdfc56f2db8827e95c94577f751a19b23
tk555 <kondo.t.555@gmail.com>
2022-04-25 17:31:43 +0900

    crkbdの方に合わせた

--- keymap.c
+++ keymap.c
@@ -14,12 +14,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
+#define MY_VLEFT LGUI(LCTL(KC_LEFT))
+#define MY_TASKV LGUI(KC_TAB)
+#define MY_VRIGHT LGUI(LCTL(KC_RIGHT))
 enum layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _NUMPAD
 };
 
 #define LOWER MO(_LOWER)
@@ -29,37 +32,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_QWERTY] = LAYOUT_planck_mit(
-    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_ESC,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LCTL,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    XXXXXXX, XXXXXXX, KC_LGUI, LOWER, KC_SPC,   KC_ENT,  RAISE,   KC_LALT,   KC_INS,  KC_PSCR,   KC_PAUS
+    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+    KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LALT,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+    KC_ESC, TG(_NUMPAD), KC_LSFT, LOWER, KC_SPC,   KC_ENT,  RAISE,  KC_LCTL ,   KC_INS,  KC_PSCR,   KC_PAUS
 ),
 
 [_LOWER] = LAYOUT_planck_mit(
-    LCTL(KC_Q)   , KC_MS_UP, KC_MS_BTN4,    KC_MS_BTN5,LCTL(KC_T), _______,   KC_APP, KC_F7, KC_F8, KC_F9, KC_F10, KC_ESC,
-    KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,    KC_MS_BTN1, KC_MS_BTN2,_______,   KC_PGUP, KC_F4, KC_F5, KC_F6, KC_F11, KC_F14,
-    LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C),    LCTL(KC_V), LCTL(KC_B),_______,   KC_PGDN, KC_F1, KC_F2, KC_F3, KC_F12, KC_F13,
-    _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    LCTL(KC_Q) ,LCTL(KC_W),LCTL(KC_E),LCTL(KC_R),LCTL(KC_T), KC_INS,   KC_APP, KC_F7, KC_F8, KC_F9, KC_F10, KC_ESC,
+    LCTL(KC_A), LCTL(KC_S),LCTL(KC_D), LCTL(KC_F),LCTL(KC_G),_______,   KC_PGUP, KC_F4, KC_F5, KC_F6, KC_F11, KC_F14,
+    LCTL(KC_Z), LCTL(KC_X),LCTL(KC_C),LCTL(KC_V), LCTL(KC_B),_______,   KC_PGDN, KC_F1, KC_F2, KC_F3, KC_F12, KC_F13,
+    _______, _______, _______, _______, _______, LSFT(LCTL(KC_ENT)), _______,    _______,    _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_mit(
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
-      KC_F20,   KC_F21,   KC_F22,  KC_F23,   KC_F24,_______,   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_MINS,  KC_EQL,
+     MY_VLEFT,  MY_TASKV,  MY_VRIGHT, _______,______,_______,KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_MINS,  KC_EQL,
       KC_F15,   KC_F16,   KC_F17,  KC_F18,   KC_F19,_______,   KC_BSPC,  KC_DEL, KC_HOME,  KC_END,  KC_GRV, KC_RSFT,
-    _______, _______, _______, _______, KC_TAB, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, KC_TAB, _______, _______, _______, _______, _______, _______
 ),
 
 
 [_ADJUST] = LAYOUT_planck_mit(
-      RESET,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, _______, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
+      RESET,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, _______, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+
+[_NUMPAD] = LAYOUT_planck_mit(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, KC_PAST, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_BSPC, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, KC_PPLS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_PSLS, KC_PDOT,
+    _______, _______, _______, _______, _______, _______, _______, _______,  KC_COMM, _______, _______
 )
 
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    HSV hsv = {120,100, 0};
+    HSV mz={HSV_MAGENTA};
+    if(layer_state_is(_NUMPAD)){
+      hsv = mz;
+    }
+
+    if (hsv.v > rgb_matrix_get_val()) {
+        hsv.v = rgb_matrix_get_val();
+    }
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(37,rgb.r,rgb.g,rgb.b);
 }
 
